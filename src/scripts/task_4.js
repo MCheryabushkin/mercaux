e9b16abe0a439c04440e94b0649981bb796dc0c6
MCheryabushkin <ch.maks@list.ru>
2020-12-21 20:10:02 +0300

    Task 8

--- src/scripts/task_4.js
+++ src/scripts/task_4.js
@@ -4,40 +4,48 @@ const sliderContainer = document.querySelector(".slider__body");
 
 const getActiveSlide = () => document.querySelector(".slider-item--visible");
 const prevSlide = () => {
-    const activeSlide = getActiveSlide();
+  const activeSlide = getActiveSlide();
+  if (activeSlide) {
     const prevSlide = activeSlide.previousElementSibling;
-
+  
     activeSlide.classList.remove("slider-item--visible");
     if (prevSlide) {
-        prevSlide.classList.add("slider-item--visible");
+      prevSlide.classList.add("slider-item--visible");
     } else {
-        sliderContainer.lastElementChild.classList.add("slider-item--visible");
+      sliderContainer.lastElementChild.classList.add("slider-item--visible");
     }
+  }
 };
 const nextSlide = () => {
-    const activeSlide = getActiveSlide();
+  const activeSlide = getActiveSlide();
+  if (activeSlide) {
     const nextSlide = activeSlide.nextElementSibling;
-    
+  
     activeSlide.classList.remove("slider-item--visible");
     if (nextSlide) {
-        nextSlide.classList.add("slider-item--visible");
+      nextSlide.classList.add("slider-item--visible");
     } else {
-        sliderContainer.firstElementChild.classList.add("slider-item--visible");
+      sliderContainer.firstElementChild.classList.add("slider-item--visible");
     }
+  }
 };
 
 let sliderInterval = setInterval(nextSlide, 2000);
-prevButton.addEventListener("click", (el) => prevSlide());
-nextButton.addEventListener("click", (el) => nextSlide());
-sliderContainer.addEventListener("click", (el) => nextSlide());
 
-/*
+if (prevButton && nextButton && sliderContainer) {
+  prevButton.addEventListener("click", (el) => prevSlide());
+  nextButton.addEventListener("click", (el) => nextSlide());
+  sliderContainer.addEventListener("click", (el) => nextSlide());
+
+  /*
 When the cursor is over the slide or navigation buttons - automatic scrolling stops
 */
 
-sliderContainer.onmouseover = () => clearInterval(sliderInterval);
-sliderContainer.onmouseout = () => sliderInterval = setInterval(nextSlide, 2000);
-prevButton.onmouseover = () => clearInterval(sliderInterval);
-prevButton.onmouseout = () => sliderInterval = setInterval(nextSlide, 2000);
-nextButton.onmouseover = () => clearInterval(sliderInterval);
-nextButton.onmouseout = () => sliderInterval = setInterval(nextSlide, 2000);
+  sliderContainer.onmouseover = () => clearInterval(sliderInterval);
+  sliderContainer.onmouseout = () =>
+    (sliderInterval = setInterval(nextSlide, 2000));
+  prevButton.onmouseover = () => clearInterval(sliderInterval);
+  prevButton.onmouseout = () => (sliderInterval = setInterval(nextSlide, 2000));
+  nextButton.onmouseover = () => clearInterval(sliderInterval);
+  nextButton.onmouseout = () => (sliderInterval = setInterval(nextSlide, 2000));
+}
