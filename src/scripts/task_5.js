ff78c65cef6225bb6043a4e95ee428303e6418d5
MCheryabushkin <ch.maks@list.ru>
2020-12-23 19:46:21 +0300

    Tasks 7 and 5

--- src/scripts/task_5.js
+++ src/scripts/task_5.js
@@ -1,7 +1,11 @@
 const header = document.querySelector(".header");
+const image = document.querySelector("img");
+
 const onWheel = (e) => {
   if (window.pageYOffset > 0) {
     header.classList.add("header--scroll");
+		header.style.transition = "all 1s";
+		image.style.transition = "all 1s";
   } else {
     header.classList.remove("header--scroll");
   }
@@ -11,7 +15,6 @@ if (header) {
   if (window.pageYOffset > 0) header.classList.add("header--scroll");
 
   document.onwheel = (delta) => {
-      console.log(delta);
-    onWheel(delta);
+    onWheel(delta); 
   };
 }
